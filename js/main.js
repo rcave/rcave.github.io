4f717affcea0698e59139fc1381cc271b28c312e
Rodulfo Caveda <rudycaveda@gmail.com>
2016-02-22 23:15:41 -0500

    lotto Changes :)

--- js/main.js
+++ js/main.js
@@ -1,30 +1,3 @@
-//function checkWidth(init) {
-//    /*If browser resized, check width again */
-//    if ($(window).width() > 500) {
-//        $('nav').show();
-//        $('nav').removeClass('active');
-//        $('nav ul li').removeClass('dropDown');
-//    } else {
-//        if (!init) {
-//            $('nav').hide();
-//            $('nav').removeClass('active');
-//            $('nav ul li').addClass('dropDown');
-//        }
-//    }
-//}
-
-//    checkWidth(true);
-//
-//    $(window).resize(function () {
-//        checkWidth(false);
-//    });
-
-
-//wait for animation to allow hover
-
-
-
-
 $('document').ready(function () {
 
     setTimeout(function () {
@@ -71,8 +44,38 @@ $('document').ready(function () {
         }
     });
 
+    //Return to top menu changes
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 10) {
+            $('#returnTop img').attr('src', 'images/322-circle-up.png');
+            $('#returnTop a').attr('href', '#home');
+        } else {
+            $('#returnTop img').attr('src', 'images/324-circle-down.png');
+            $('#returnTop a').attr('href', '#bio');
 
+        }
+    });
+    var hashTagActive = "";
+    $(".scroll").click(function (event) {
+        if(hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
+            event.preventDefault();
+            //calculate destination place
+            var dest = 0;
+            if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+                dest = $(document).height() - $(window).height();
+            } else {
+                dest = $(this.hash).offset().top;
+            }
+            //go to destination
+            $('html,body').animate({
+                scrollTop: dest
+            }, 1000, 'ease');
+            hashTagActive = this.hash;
+        }
+    });
+    
 
+    //Clicking Menu items
     $('.hexLinks li a').click(function () {
         $('#navTakeOver').slideUp(1000);
         setTimeout(function () {
@@ -87,4 +90,53 @@ $('document').ready(function () {
             }, 1000);
         }
     });
+
+    //FACEBOOK HOVER
+    $('#contactFacebook').mouseenter(function () {
+        $('.facebookLogo').removeClass('socialInactive');
+    });
+    $('#contactFacebook').mouseleave(function () {
+        $('.facebookLogo').addClass('socialInactive');
+    });
+
+    //INSTAGRAM HOVER 
+    $('#contactInstagram').mouseenter(function () {
+        $('.InstagramLogo').removeClass('socialInactive');
+    });
+    $('#contactInstagram').mouseleave(function () {
+        $('.InstagramLogo').addClass('socialInactive');
+    });
+
+    //LINKEDIN HOVER
+    $('#contactLinkedIn').mouseenter(function () {
+        $('.LinkedInLogo').removeClass('socialInactive');
+    });
+    $('#contactLinkedIn').mouseleave(function () {
+        $('.LinkedInLogo').addClass('socialInactive');
+    });
+
+    //GITHUBHOVER
+    $('#contactGithub').mouseenter(function () {
+        $('.GitHubLogo').removeClass('socialInactive');
+    });
+    $('#contactGithub').mouseleave(function () {
+        $('.GitHubLogo').addClass('socialInactive');
+    });
+
+    //CODEPEN HOVER
+    $('#contactCodePen').mouseenter(function () {
+        $('.CodePenLogo').removeClass('socialInactive');
+    });
+    $('#contactCodePen').mouseleave(function () {
+        $('.CodePenLogo').addClass('socialInactive');
+    });
+
+    //SPOTIFY HOVER
+    $('#contactSpotify').mouseenter(function () {
+        $('.SpotifyLogo').removeClass('socialInactive');
+    });
+    $('#contactSpotify').mouseleave(function () {
+        $('.SpotifyLogo').addClass('socialInactive');
+    });
+
 });
