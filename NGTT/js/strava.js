39f2176542f5983e1d92643a1daf335bc39083bd
Rodulfo Caveda <rudycaveda@gmail.com>
2016-05-03 19:17:25 -0400

    AddingID

--- NGTT/js/strava.js
+++ NGTT/js/strava.js
@@ -9,10 +9,8 @@
 console.log("Strava Script Loaded");
 
 $.ajax({
-    url: "https://www.strava.com/api/v3/athletes/7863323",
-    headers: {
-        'Autorization' : 'Bearer c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3'
-    },
+    url: "https://www.strava.com/api/v3/athletes/786332",
+     headers: { 'Authorization': 'Bearer c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3' },
     jsonp: "callback",
     dataType: "jsonp", 
     data: {format: "json"}, 
