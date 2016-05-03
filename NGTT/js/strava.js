55b3626a8b28674785d72971767d9cdb1dc58910
Rudy Caveda <rudycaveda@gmail.com>
2016-05-03 14:49:27 -0400

    adding ajax Get call

--- NGTT/js/strava.js
+++ NGTT/js/strava.js
@@ -2,4 +2,19 @@
 /*** STRAVA API CALLS ***/
 /************************/
 
+
+// client ID: "9192"
+// Client Secret: "9d563e863adc54e8e3d719aa78cb141cc3b068de"
+// accessToken: "c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3"
 console.log("Strava Script Loaded");
+
+$.ajax({
+    url: "https://www.strava.com/api/v3/athlete",
+    jsonp: "callback",
+    dataType: "jsonp", 
+    data: {format: "json"}, 
+    success: function( response ) {
+        console.log(response); //server response
+    }
+
+});
