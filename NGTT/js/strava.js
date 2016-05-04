5f3c22613628c74c8e810d592252998aabf26723
Rudy Caveda <rudycaveda@gmail.com>
2016-05-04 11:24:19 -0400

    changing get call

--- NGTT/js/strava.js
+++ NGTT/js/strava.js
@@ -13,16 +13,15 @@ $(document).ready(function () {
     $.ajax({
         type: "get",
         dataType: "jsonp",
-        url: "https://www.strava.com/api/v3/athletes/786332?access_token=c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3",
+        url: "https://www.strava.com/api/v3/athlete/activities?access_token=c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3",
         success: function (data, response) {
-            console.log('FINALLY FUCKING DID IT!');
-            console.log(data);
             console.log(response);
+            console.log(data);
 
         },
         error: function () {
             console.log('Uh Oh!');
         },
     });
-    
+
 });
