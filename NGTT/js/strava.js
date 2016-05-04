1588600729c6cb8124d9e74bc51ff9b28d72c0c4
Rodulfo Caveda <rudycaveda@gmail.com>
2016-05-03 23:14:02 -0400

    removed some stuff

--- NGTT/js/strava.js
+++ NGTT/js/strava.js
@@ -9,14 +9,12 @@
 console.log("Strava Script Loaded");
 
 var settings = {
-  "async": false,
+  "async": true,
   "crossDomain": true,
   "url": "https://www.strava.com/api/v3/athletes/786332",
   "method": "GET",
   "headers": {
-    "authorization": "Bearer c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3",
-    "cache-control": "no-cache",
-    "postman-token": "b6d237e9-ee6a-7ced-bd6d-55340875b7b7"
+    "authorization": "Bearer c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3"
   }
 }
 
