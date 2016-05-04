4b2c900764639a9995e1f2b4823668759191c1a7
Rudy Caveda <rudycaveda@gmail.com>
2016-05-04 08:57:26 -0400

    Changing AJAX request

--- NGTT/js/strava.js
+++ NGTT/js/strava.js
@@ -9,15 +9,23 @@
 console.log("Strava Script Loaded");
 
 $(document).ready(function () {
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://www.strava.com/api/v3/athletes/786332?access_token=c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3",
-        "method": "GET",
-        "dateType": "jsonp",
-    }
+    //    var settings = {
+    //        "async": true,
+    //        "crossDomain": true,
+    //        "url": "https://www.strava.com/api/v3/athletes/786332?access_token=c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3",
+    //        "method": "GET",
+    //        "dateType": "jsonp",
+    //    }
+    //
+    //    $.ajax(settings).done(function (response) {
+    //        console.log(response);
+    //    });
 
-    $.ajax(settings).done(function (response) {
-        console.log(response);
+    $.ajax({
+        url: "https://www.strava.com/api/v3/athletes/786332?access_token=c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3",
+        jsonp: "$jsonp",
+        dataType: "jsonp"
+    }).done(function (data) {
+        console.log("Request received: " + data);
     });
 });
