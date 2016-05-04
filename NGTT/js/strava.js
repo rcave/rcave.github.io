49d5701edfb6a285ea25899dbad5cf8ab232fa96
Rodulfo Caveda <rudycaveda@gmail.com>
2016-05-03 23:15:27 -0400

    xhr request

--- NGTT/js/strava.js
+++ NGTT/js/strava.js
@@ -8,16 +8,20 @@
 // accessToken: "c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3"
 console.log("Strava Script Loaded");
 
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://www.strava.com/api/v3/athletes/786332",
-  "method": "GET",
-  "headers": {
-    "authorization": "Bearer c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3"
-  }
-}
+var data = null;
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === 4) {
+    console.log(this.responseText);
+  }
 });
+
+xhr.open("GET", "https://www.strava.com/api/v3/athletes/786332");
+xhr.setRequestHeader("authorization", "Bearer c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3");
+xhr.setRequestHeader("cache-control", "no-cache");
+xhr.setRequestHeader("postman-token", "eed0ae1a-2cdf-71b8-3968-39f90ec96051");
+
+xhr.send(data);
