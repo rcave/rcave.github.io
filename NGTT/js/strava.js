63cc323a192dee16c8fe62596743b7463b175966
Rodulfo Caveda <rudycaveda@gmail.com>
2016-05-03 22:59:42 -0400

    Header to JSON[

--- NGTT/js/strava.js
+++ NGTT/js/strava.js
@@ -8,37 +8,17 @@
 // accessToken: "c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3"
 console.log("Strava Script Loaded");
 
-////var xhr = new XMLHttpRequest();
-//
-//$.ajaxSetup({
-//    beforeSend: function(xhr) {
-//        xhr.setRequestHeader('Authorization', 'Bearer c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3');
-//    }
-//});
-//
-//
-//$.ajax({
-//    url: "https://www.strava.com/api/v3/athletes/786332",
-//    //jsonp: "callback",
-//    dataType: "jsonp",
-//    data: {
-//        format: "json"
-//    },
-//    success: function (response) {
-//        console.log(response); //server response
-//    },
-//});
-
-var accessToken = "c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3";
-
 $.ajax({
-  url: "https://www.strava.com/api/v3/athlete",
-  dataType: 'jsonp',
-  data: {
-    alt: 'json-in-script'
-  },
-  success: function(data, status) {
-    return console.log("The returned data", data);
-  },
-  beforeSend: function(xhr) { xhr.setRequestHeader('Authorization','Bearer c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3'); } 
+    headers: {
+        "Authorization": "Bearer c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3"
+    },
+    url: "https://www.strava.com/api/v3/athletes/786332",
+    jsonp: "callback",
+    dataType: "jsonp",
+    data: {
+        format: "json"
+    },
+    success: function (response) {
+        console.log(response); //server response
+    }
 });
