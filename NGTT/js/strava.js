92578f07fd899bf0f56fc150d90d030f0fae8b4b
Rudy Caveda <rudycaveda@gmail.com>
2016-05-04 10:16:04 -0400

    Test

--- NGTT/js/strava.js
+++ NGTT/js/strava.js
@@ -13,12 +13,11 @@ $(document).ready(function () {
     var jsonData;
 
     $.ajax({
-        url: "https://www.strava.com/api/v3/athletes/786332?access_token=c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3",
+        url: "https://www.strava.com/api/v3/athletes/786332?access_token=c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3&format=jsonp",
         type: "get",
         crossDomain: true,
         dataType: "jsonp",
-        jsonp: "jsonp",
-        callback: "JSON_CALLBACK",
+        jsonp: "json_callback",
         success: function () {
             console.log('Success!');
         },
