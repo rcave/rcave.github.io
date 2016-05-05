613818272f00a8378e4d895fe31a41d7086d44fe
Rodulfo Caveda <rudycaveda@gmail.com>
2016-05-04 21:38:57 -0400

    Adding Strava API calls to NGTT Home

--- NGTT/js/strava.js
+++ NGTT/js/strava.js
@@ -8,23 +8,45 @@
 // accessToken: "c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3"
 console.log("Strava Script Loaded");
 
-$(document).ready(function () {
+$(document).ready(function() {
 
-    $.ajax({
-        type: "get",
-        dataType: "jsonp",
-        url: "https://www.strava.com/api/v3/athletes/786332?access_token=c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3",
-        success: function (data, response) {
-            console.log(response);
-            console.log(data);
+  $.ajax({
+    type: "get",
+    dataType: "jsonp",
+    url: "https://www.strava.com/api/v3/athletes/786332?access_token=c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3",
+    success: function(data, response) {
+      console.log(response);
+      console.log(data);
+      $('.riderId').append(data.id);
+      $('.fn').append(data.firstname);
+      $('.ln').append(data.lastname);
+      $('.riderProfile img').attr('src', data.profile);
+    },
+    error: function(response) {
+      console.log('Uh Oh!');
+      console.log(response);
 
-            var athleteData = data;
-            
-            $('.stravaData').append("<p>" + athleteData + "</p>");
-        },
-        error: function () {
-            console.log('Uh Oh!');
-        },
-    });
+    }
+  });
+  
+  $.ajax({
+    type: "get",
+    dataType: "jsonp",
+    url: "https://www.strava.com/api/v3/athletes/786332/stats?access_token=c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3",
+    success: function(data, response) {
+      console.log(response);
+      console.log(data);
+      $('.ytdCount span').append(data.ytd_ride_totals.count);
+      var ytdDistance = data.ytd_ride_totals.distance * 0.000621371;
+      var fixed = ytdDistance.toFixed(1);
+      $('.ytdDistance span').append(fixed);
+
+    },
+    error: function(response) {
+      console.log('Uh Oh!');
+      console.log(response);
+
+    }
+  });
 
 });
