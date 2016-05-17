8ed449a3cf605bb9beb3e63bfe44b184d504e439
Rudy Caveda <rudycaveda@gmail.com>
2016-05-17 11:00:08 -0400

    adding creds

--- Jira/jira.js
+++ Jira/jira.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
 
     $.ajax({
-        type: "get",
-        dataType: "json",
-        url: "https://rudy.caveda:Snare7192@bluesombrero.atlassian.net/jira/rest/api/2/search?jql=asignee=rudy.caveda",
+        type: 'get',
+        dataType: 'json',
+        url: 'https://bluesombrero.atlassian.net/jira/rest/api/2/search?jql=asignee=rudy.caveda',
+        username: 'rudy.caveda',
+        password: 'Snare7192', 
+        xhrFields: { withCredentials: true },
         success: function (data, response) {
             console.log(response);
             console.log(data);
