d6e736356310bc4f3989eacc0aa34fd2df9f5498
Rudy Caveda <rudycaveda@gmail.com>
2016-05-23 08:18:04 -0400

    reverting back

--- Jira/jira.js
+++ Jira/jira.js
@@ -1,26 +1,20 @@
 $(document).ready(function () {
 
-    //    $.ajax({
-    //        type: 'get',
-    //        dataType: 'json',
-    //        url: 'https://bluesombrero.atlassian.net/jira/rest/api/2/search?jql=asignee=rudy.caveda',
-    //        username: 'rudy.caveda',
-    //        password: 'Snare7192', 
-    //        xhrFields: { withCredentials: true },
-    //        success: function (data, response) {
-    //            console.log(response);
-    //            console.log(data);
-    //        },
-    //        error: function (response) {
-    //            console.log('Uh Oh!');
-    //            console.log(response);
-    //        }
-    //    });
+        $.ajax({
+            type: 'get',
+            dataType: 'json',
+            url: 'https://bluesombrero.atlassian.net/jira/rest/api/2/search?jql=asignee=rudy.caveda',
+            username: 'rudy.caveda',
+            password: 'Snare7192', 
+            xhrFields: { withCredentials: true },
+            success: function (data, response) {
+                console.log(response);
+                console.log(data);
+            },
+            error: function (response) {
+                console.log('Uh Oh!');
+                console.log(response);
+            }
+        });
 
-
-    var xhr = new XMLHttpRequest();
-
-    xhr.open("GET", "https://rudy.caveda:Snare7192@bluesombrero.atlassian.net/rest/agile/1.0/issue/BWA-6507");
-    xhr.send();
-    console.log(xhr.responseText);
 });
