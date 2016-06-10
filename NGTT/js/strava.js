c8fb1acca3ba45b0e94b2890fe1284a4b66f4f79
Rodulfo Caveda <rudycaveda@gmail.com>
2016-06-09 21:03:34 -0400

    Starva.js Updated

--- NGTT/js/strava.js
+++ NGTT/js/strava.js
@@ -8,45 +8,36 @@
 // accessToken: "c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3"
 console.log("Strava Script Loaded");
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-  $.ajax({
-    type: "get",
-    dataType: "jsonp",
-    url: "https://www.strava.com/api/v3/athletes/786332?access_token=c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3",
-    success: function(data, response) {
-      console.log(response);
-      console.log(data);
-      $('.riderId').append(data.id);
-      $('.fn').append(data.firstname);
-      $('.ln').append(data.lastname);
-      $('.riderProfile img').attr('src', data.profile);
-    },
-    error: function(response) {
-      console.log('Uh Oh!');
-      console.log(response);
+    $.ajax({
+        type: "get",
+        dataType: "jsonp",
+        url: "https://www.strava.com/api/v3/clubs/200696/activities?access_token=c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3",
+        success: function (data, response) {
+            console.log(response);
+            console.log(data);
+            $.each(data, function (i, e) {
+                var riderImage = data[i].athlete.profile;
+                var firstName = data[i].athlete.firstname;
+                var lastName = data[i].athlete.lastname;
+                var actType = data[i].type;
+                var distance = data[i].distance;
+                var convDist = distance * 0.000621371;
+                var fixedDist = convDist.toFixed(1);
+                var avgSpd = data[i].average_speed;
+                var convSpd = avgSpd * 2.23694;
+                var fixedSpd = convSpd.toFixed(1);
+                var str = '<div class="activities"><img class="riderImage" src="' + riderImage + '"/><div class="riderInfo"><span class="riderName">' + firstName + ' ' + lastName + '</span><p>Activity Type: ' + actType + '</p><p>Distance: ' + fixedDist + ' mi</p><p>Avg Speed: ' + fixedSpd + ' mi/hr</p></div></div>';
+                $('.stravaActivities').append(str);
+            });
+        },
 
-    }
-  });
-  
-  $.ajax({
-    type: "get",
-    dataType: "jsonp",
-    url: "https://www.strava.com/api/v3/athletes/786332/stats?access_token=c0ed3db9c2a6ae04e3d23097d62735dba7bba1c3",
-    success: function(data, response) {
-      console.log(response);
-      console.log(data);
-      $('.ytdCount span').append(data.ytd_ride_totals.count);
-      var ytdDistance = data.ytd_ride_totals.distance * 0.000621371;
-      var fixed = ytdDistance.toFixed(1);
-      $('.ytdDistance span').append(fixed);
-
-    },
-    error: function(response) {
-      console.log('Uh Oh!');
-      console.log(response);
-
-    }
-  });
+        error: function (response) {
+            console.log('Uh Oh!');
+            console.log(response);
+            $('.stravaActivities').append("NOTHING WORKS");
+        }
+    });
 
 });
